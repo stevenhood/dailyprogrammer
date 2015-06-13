1d852046b8d57f43eedf5a7f413c297162625910
Steven Hood <steven.hood@live.co.uk>
2015-06-13 10:38:48 +0100

    Add testing of numbers 0-1000

--- palindromic_numbers.py
+++ palindromic_numbers.py
@@ -3,6 +3,8 @@
 # [2015-06-08] Challenge #218 [Easy] Making numbers palindromic
 # http://bit.ly/1C4nMf9
 
+from collections import defaultdict
+
 def reverse_digits(x):
     return int(str(x)[::-1])
 
@@ -14,11 +16,19 @@ def is_palindrome(s):
 def calc(origin):
     val = origin
     steps = 0
-    while not is_palindrome(val):
+    while not is_palindrome(val) and steps < 10000:
         val += reverse_digits(val)
         steps += 1
-    print('%d gets palindromic after %d steps: %d' % (origin, steps, val))
+    if is_palindrome(val):
+        print('%d gets palindromic after %d steps: %d' % (origin, steps, val))
+        return val
+    return None
 
 calc(123)
 calc(286)
 calc(196196871)
+
+palins = defaultdict(list)
+for n in range(1001):
+    key = calc(n)
+    palins[key].append(n)
